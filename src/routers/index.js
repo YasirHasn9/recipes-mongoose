a052ad4375879e8e1359a7257f63ce0d23dea266
Yasir Hamm <yasir@Yasirs-MacBook-Pro.local>
2021-11-02 13:52:32 -0700

    get a recipes by its name

--- src/routers/index.js
+++ src/routers/index.js
@@ -11,7 +11,7 @@ router.get("/", async (req, res) => {
 	}
 });
 
-router.get("/recipeNames", async (req, res) => {
+router.get("/names", async (req, res) => {
 	try {
 		const result = await db.find();
 		const recipeNames = await result.map(r => r.name);
@@ -20,6 +20,27 @@ router.get("/recipeNames", async (req, res) => {
 		});
 	} catch (err) {
 		console.log(err);
+	}
+});
+
+router.get("/:name", async (req, res) => {
+	const { name } = req.params;
+	try {
+		const recipes = await db.find();
+		for (const rec of recipes) {
+			if (rec.name === name) {
+				const ingredients = rec["ingredients"];
+				const len = ingredients.length;
+				res.send({
+					details: {
+						ingredients,
+						numSteps: len,
+					},
+				});
+			}
+		}
+	} catch (err) {
+		res.send("err", err);
 	}
 });
 module.exports = router;
