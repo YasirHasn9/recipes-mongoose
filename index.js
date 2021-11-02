dc1465b220af41c594ef9c33fa01354220aee1d3
Yasir Hamm <yasir@Yasirs-MacBook-Pro.local>
2021-11-02 13:12:51 -0700

    handling not-found requests

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 require("dotenv").config();
 const port = process.env.PORT;
 const server = require("./src/index");
+server.use(function (req, res) {
+	res.status(404);
 
+	// respond with json
+	if (req.accepts("json")) {
+		res.json({ error: "Not found" });
+		return;
+	}
+
+	// default to plain-text. send()
+	res.type("txt").send("Not found");
+});
 server.listen(port, () => {
 	console.log(`http://localhost:${port}`);
 });
