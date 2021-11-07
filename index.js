b4c38f47d76181f32be02f8a9985b4a7d5b8fe30
Yasir Hamm <yasir@Yasirs-MacBook-Pro.local>
2021-11-07 01:39:17 -0800

    make index.js the main file

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
 const port = process.env.PORT;
+const uri = process.env.URI;
+const mongoose = require("mongoose");
+
+//connect to the db
+mongoose.connect(uri);
+
+// get the server
 const server = require("./src/index");
-server.use(function (req, res) {
-	res.status(404);
+server.use(express.json());
+// parse the request resource and put on the request object
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+// restrict the web from accessing different web servers and domains
+server.use(cors());
+server.use(helmet());
 
-	// respond with json
-	if (req.accepts("json")) {
-		res.json({ error: "Not found" });
-		return;
-	}
+// server.use(function (req, res) {
+// 	res.status(404);
 
-	// default to plain-text. send()
-	res.type("txt").send("Not found");
+// 	// respond with json
+// 	if (req.accepts("json")) {
+// 		res.json({ error: "Not found" });
+// 		return;
+// 	}
+
+// 	// default to plain-text. send()
+// 	res.type("txt").send("Not found");
+// });
+
+server.use("/hey", (req, res) => {
+	res.status(200).json({
+		message: "Welcome to the recipes api",
+	});
 });
 server.listen(port, () => {
 	console.log(`http://localhost:${port}`);
