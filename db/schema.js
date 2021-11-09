d9f0d7bc64b998946e4159892e312258a319cc82
Yasir Hamm <yasir@Yasirs-MacBook-Pro.local>
2021-11-09 13:06:50 -0800

    validated schema types

--- db/schema.js
+++ db/schema.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-
 const recipesSchema = mongoose.Schema({
 	name: {
 		type: String,
@@ -8,7 +7,18 @@ const recipesSchema = mongoose.Schema({
 		lowercase: true,
 		minLength: 2,
 	},
-	ingredients: { type: [String], require: true, minLength: 1 },
-	instructions: { type: [String], require: true, minLength: 1 },
+	ingredients: { type: [String], require: true, minItems: 1 },
+	instructions: { type: [String], require: true, minItems: 1 },
+});
+
+recipesSchema.path("ingredients").validate(value => {
+	if (value.length < 1) {
+		throw new Error("ingredients");
+	}
+});
+recipesSchema.path("instructions").validate(value => {
+	if (value.length < 1) {
+		throw new Error("instructions");
+	}
 });
 module.exports = mongoose.model("Recipes", recipesSchema);
