b70a7ce184f6950fa980eafe8111c6b8350db237
Yasir Hamm <yasir@Yasirs-MacBook-Pro.local>
2021-11-08 21:53:00 -0800

    create new file for dummy seed data

--- db/schema.js
+++ db/schema.js
@@ -12,13 +12,3 @@ const recipesSchema = mongoose.Schema({
 	instructions: { type: [String], require: true, minLength: 1 },
 });
 module.exports = mongoose.model("Recipes", recipesSchema);
-
-/*
-It is just an idea, what if i used the properties as a validators to other 
-properties
-const recipesSchema = mongoose.Schema({
-	name: { type: String, require: true },
-	ingredients: { type: [String], require: () => this.name.length > 0},
-	instructions: { type: [String], require: () => this.name.length > 0 },
-});
-*/
--- db/seeds.js
+++ db/seeds.js
@@ -1,52 +1,6 @@
 const mongoose = require("mongoose");
-const RecipesDb = require("./schema");
+const seeds = require("./recipesData");
 const uri = process.env.URI;
-
-const recipes = [
-	new RecipesDb({
-		name: "",
-		ingredients: ["1 tsp oil", "2 eggs", "salt"],
-		instructions: [
-			"Beat eggs with salt",
-			"Heat oil in pan",
-			"Add eggs to pan when hot",
-			"Gather eggs into curds, remove when cooked",
-			"Salt to taste and enjoy",
-		],
-	}),
-	new RecipesDb({
-		name: "garlicPasta",
-		ingredients: [
-			"500mL water",
-			"100g spaghetti",
-			"25mL olive oil",
-			"4 cloves garlic",
-			"Salt",
-		],
-		instructions: [
-			"Heat garlic in olive oil",
-			"Boil water in pot",
-			"Add pasta to boiling water",
-			"Remove pasta from water and mix with garlic olive oil",
-			"Salt to taste and enjoy",
-		],
-	}),
-	new RecipesDb({
-		name: "chai",
-		ingredients: [
-			"400mL water",
-			"100mL milk",
-			"5g chai masala",
-			"2 tea bags or 20 g loose tea leaves",
-		],
-		instructions: [
-			"Heat water until 80 C",
-			"Add milk, heat until 80 C",
-			"Add tea leaves/tea bags, chai masala; mix and steep for 3-4 minutes",
-			"Remove mixture from heat; strain and enjoy",
-		],
-	}),
-];
 
 mongoose
 	.connect(uri, { useNewUrlParser: true })
@@ -58,15 +12,11 @@ mongoose
 		console.log("connected to db in development environment");
 	});
 
-recipes.map(async (r, index) => {
+seeds.map(async (r, index) => {
 	await r.save((err, res) => {
-		if (err) {
-			return err;
-		} else {
-			if (index === recipes.length - 1) {
-				console.log("DONE!");
-				mongoose.disconnect();
-			}
+		if (index === seeds.length - 1) {
+			console.log("DONE!");
+			mongoose.disconnect();
 		}
 	});
 });
